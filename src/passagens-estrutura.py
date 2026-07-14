314d6af084db012d6609603bdf428d0912c96464
Karina <karinaskatto123@gmail.com>
2026-07-14 06:32:01 -0300

    Add the passsagem to the doc and cheking if a generic model with the same symbol already exists

--- src/passagens-estrutura.py
+++ src/passagens-estrutura.py
@@ -12,7 +12,7 @@ from RevitServices.Persistence import DocumentManager
 doc = DocumentManager.Instance.CurrentDBDocument
 
 
-
+#--------------------------------------------------------------------------------------------------------------------------------------
 # Get linked document
 dataEnteringNode = IN
 
@@ -22,7 +22,7 @@ link_doc = link_inst[0].GetLinkDocument()
 OUT = link_inst
 
 
-
+#--------------------------------------------------------------------------------------------------------------------------------------
 # Get all pipes from the linked document
 dataEnteringNode = IN
 
@@ -38,7 +38,7 @@ for link in link_inst:
 OUT = pipes
 
 
-
+#--------------------------------------------------------------------------------------------------------------------------------------
 # Get all walls from the host document
 dataEnteringNode = IN
 
@@ -49,66 +49,67 @@ host_walls = (FilteredElementCollector(doc)
 OUT = host_walls
 
 
-
+#--------------------------------------------------------------------------------------------------------------------------------------
 # Get solids from elements
 dataEnteringNode = IN
 
 def get_solids(elements):
-    solids = [] 
+    data = [] 
     for el in elements: 
         el = UnwrapElement(el)
+        try: 
+            d = el.Diameter
+        except:
+            d = None
         try:
-            geometry = el.get_Geometry(Options())
-            for g in geometry: 
+            for g in el.get_Geometry(Options()): 
                 if isinstance(g, Solid) and g.Volume > 0:
-                    solids.append(g)
+                    data.append((g, d))
         except Exception: 
             pass 
-    return solids     
+    return data     
 
-wall_solids = get_solids(IN[0])
-pipe_solids = get_solids(IN[1])
-OUT = pipe_solids, wall_solids 
+wall_data = get_solids(IN[1])
+pipe_data = get_solids(IN[0])
+OUT = (wall_data, pipe_data)
 
 
 
+#--------------------------------------------------------------------------------------------------------------------------------------
 # Move pipe solids to the hostdocument's coordinate system
-pipe_solids = IN[0][0]    
-wall_solids = IN[0][1]     
+pipe_data = IN[0][1]        
 
 link = UnwrapElement(IN[1])[0]
 transform = link.GetTotalTransform()
 moved_pipes = []
-for s in pipe_solids:
-    new_solid = SolidUtils.CreateTransformed(s, transform)
-    moved_pipes.append(new_solid)
+for solid, d in pipe_data:
+    new_solid = SolidUtils.CreateTransformed(solid, transform)
+    moved_pipes.append((new_solid, d))
 
 OUT = moved_pipes
 
 
-
+#--------------------------------------------------------------------------------------------------------------------------------------
 # Check for clashes between moved pipe solids and wall solids
-   
-wall_solids = IN[0][1]  
+wall_data = IN[0][1]  
 moved_pipes = IN[1]
 
 clashes = []
-for pipe in moved_pipes:
-    for wall in wall_solids:
+for pipe_solid, d in moved_pipes:
+    for wall_solid, _ in wall_data:
         try:
             result = BooleanOperationsUtils.ExecuteBooleanOperation(
-                pipe, wall, BooleanOperationsType.Intersect)
+                pipe_solid, wall_solid, BooleanOperationsType.Intersect)
             if result.Volume > 0:
-                clashes.append([pipe, wall])
+                clashes.append([pipe_solid, wall_solid, d])
         except:
             pass
             
 OUT = clashes
 
 
-
+#--------------------------------------------------------------------------------------------------------------------------------------
 # Get centroids of clashes
-   
 clashes = IN[0]
 
 centroids = []
@@ -121,3 +122,63 @@ for clashe in clashes:
     centroids.append(point)
 
 OUT = centroids
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+# Create family instances at the clash centroids
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Structure import StructuralType
+clr.AddReference('RevitServices')
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+
+insert_points = IN[0]
+symbol = UnwrapElement(IN[1])
+
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+if not symbol.IsActive:
+    symbol.Activate()
+    doc.Regenerate()
+
+passagem = []
+for in_point in insert_points:  
+    fi = doc.Create.NewFamilyInstance(in_point, symbol, StructuralType.NonStructural)
+    passagem.append(fi) 
+
+TransactionManager.Instance.TransactionTaskDone()
+
+OUT = passagem
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+# Get all generic models from the host document
+dataEnteringNode = IN
+
+generic_model = (FilteredElementCollector(doc)
+.OfCategory(BuiltInCategory.OST_GenericModel)
+.WhereElementIsNotElementType().ToElements())
+
+OUT = generic_model
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+# Check if a generic model with the same symbol already exists
+generic_exist = UnwrapElement(IN[0])
+symbol = UnwrapElement(IN[1])
+
+pass_exist = []
+for g in generic_exist:
+    try:
+        if g.Symbol.Id == symbol.Id:
+            pass_exist.append(g)
+    except:
+        pass
+
+OUT = pass_exist
